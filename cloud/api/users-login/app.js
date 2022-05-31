6f877cb0c6e5e7eff258dda10cd91328be034ec7
Tarsis de Lima <br.tmvdl@gmail.com>
2022-05-31 15:47:40 -0300

    Adding '/user/login' endpoint

---
+++ cloud/api/users-login/app.js
@@ -0,0 +1,29 @@
+const { NotFoundError } = require('/mercado/commons/errors')
+const db = require('/mercado/commons/db')
+const loginIndex = db.in('logins')
+const userIndex = db.in('users')
+
+const usersCreate = ({ name = '', email }) => {
+  const user = userIndex.new()
+  user.writeMany({ name, email })
+  return user
+}
+
+module.exports = ({ body: { email } }, res) => {
+  let user = userIndex.find({ email })
+
+  if (!user) {
+    user = usersCreate({ email })
+  }
+
+  const login = loginIndex.new()
+
+  login.writeMany({
+    user_id: user.getId(),
+    created_at: Date.now().toString(),
+  })
+
+  return res.json({ token: login.getId() })
+}
+
+module.exports.usersCreate = usersCreate
