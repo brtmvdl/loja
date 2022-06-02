4fe939811e1d19dcf65a543240fe464309631ff0
Tarsis de Lima <br.tmvdl@gmail.com>
2022-06-01 23:13:49 -0300

    Adding '/users/login' endpoint

--- cloud/api/users-login/app.js
+++ cloud/api/users-login/app.js
@@ -1,28 +1,19 @@
-const db = require('/mercado/commons/db')
+const db = require('/julia_store/commons/db')
 const loginIndex = db.in('logins')
 const userIndex = db.in('users')
 
-const usersCreate = ({ name = '', email }) => {
-  const user = userIndex.new()
-  user.writeMany({ name, email })
-  return user
-}
+const usersCreate = ({ email }) =>
+  userIndex.new().writeMany({ email })
 
 module.exports = ({ body: { email } }, res) => {
-  let user = userIndex.find({ email })
+  let user = userIndex.select({ email })
 
-  if (!user) {
-    user = usersCreate({ email })
-  }
+  if (!user) user = usersCreate({ email })
 
   const login = loginIndex.new()
-
-  login.writeMany({
-    user_id: user.getId(),
-    created_at: Date.now().toString(),
-  })
+  const user_id = user.getId()
+  const created_at = Date.now().toString()
+  login.writeMany({ user_id, created_at })
 
   return res.json({ token: login.getId() })
 }
-
-module.exports.usersCreate = usersCreate
