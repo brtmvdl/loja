7e5ae19dc8755780332845c689f5a624a2dacfdf
Tarsis de Lima <br.tmvdl@gmail.com>
2022-06-01 23:14:14 -0300

    Adding '/providers/list' endpoint

---
+++ cloud/api/providers-list/app.js
@@ -0,0 +1,12 @@
+const db = require('/julia_store/commons/db')
+const providersIndex = db.in('providers')
+const loginsIndex = db.in('logins')
+
+module.exports = ({ headers: { login } }, res) => {
+  const loginDB = loginsIndex.get(login)
+  const user_id = loginDB.readString('user_id')
+
+  return res.json({
+    list: providersIndex.selectJSON({ user_id })
+  })
+}
