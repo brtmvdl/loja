f688be3ebe6fb7db5caf5f309361024f0e5187df
Tarsis de Lima <br.tmvdl@gmail.com>
2022-06-01 23:14:22 -0300

    Adding '/providers/create' endpoint

---
+++ cloud/api/providers-create/app.js
@@ -0,0 +1,17 @@
+const { NotFoundError } = require('/julia_store/commons/errors')
+const db = require('/julia_store/commons/db')
+const providersIndex = db.in('providers')
+const loginsIndex = db.in('logins')
+
+module.exports = ({ body: { name }, headers: { login } }, res) => {
+  const loginDB = loginsIndex.get(login)
+  if (!loginDB) throw new NotFoundError('Login not found.')
+
+  const user_id = loginDB.readString('user_id')
+  const created_at = Date.now().toString()
+
+  const provider = providersIndex.new()
+  provider.writeMany({ name, created_at, user_id })
+
+  return res.json({ id: provider.getId(), created_at })
+}
