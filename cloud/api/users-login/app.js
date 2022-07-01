6acc5207bc3dcdb32539d8dfef00f5bfc7c52ba3
Tarsis de Lima <br.tmvdl@gmail.com>
2022-07-01 12:03:52 -0300

    Fixing '/users/login' endpoint

--- cloud/api/users-login/app.js
+++ cloud/api/users-login/app.js
@@ -1,19 +1,23 @@
 const db = require('/julia_store/commons/db')
+const { NotFoundError } = require('/julia_store/commons/errors')
 const loginIndex = db.in('logins')
 const userIndex = db.in('users')
 
-const usersCreate = ({ email }) =>
-  userIndex.new().writeMany({ email })
+module.exports = ({ body: { email, password } }, res) => {
+  const user = userIndex.selectOne({ email, password })
 
-module.exports = ({ body: { email } }, res) => {
-  let user = userIndex.select({ email })
+  if (!user) {
+    return res.error(new NotFoundError('User not found.', { email }))
+  }
 
-  if (!user) user = usersCreate({ email })
-
-  const login = loginIndex.new()
-  const user_id = user.getId()
   const created_at = Date.now().toString()
-  login.writeMany({ user_id, created_at })
+  const login = loginIndex.new().writeMany({
+    user_id: user.getId(),
+    created_at,
+  })
 
-  return res.json({ token: login.getId() })
+  return res.json({
+    token: login.getId(),
+    created_at
+  })
 }
