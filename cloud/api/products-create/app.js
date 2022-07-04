e95805a9076cbc3d92594a926ee193002d56f795
Tarsis de Lima <br.tmvdl@gmail.com>
2022-07-04 13:59:05 -0300

    Adding mocked '/products/create' endpoint

---
+++ cloud/api/products-create/app.js
@@ -0,0 +1,17 @@
+const { NotFoundError } = require('/julia_store/commons/errors')
+const db = require('/julia_store/commons/db')
+const productsIndex = db.in('products')
+const loginsIndex = db.in('logins')
+
+module.exports = ({ body: { name }, headers: { login } }, res) => {
+  const loginDB = loginsIndex.selectById(login)
+  if (!loginDB) throw new NotFoundError('Login not found.')
+
+  const user_id = loginDB.readString('user_id')
+  const created_at = Date.now().toString()
+
+  const product = productsIndex.new()
+  product.writeMany({ name, created_at, user_id })
+
+  return res.json({ id: product.getId(), created_at })
+}
